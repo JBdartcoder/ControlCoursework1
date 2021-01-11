5aa32ec728334a6823e92923d74b436b16e78061
Simon-Paine <snpaine@btinternet.com>
2021-01-11 12:06:03 +0000

    Tidied up code and added a function to sub in constant values - makes code a bit shorted and neater

--- problemB2_linearised.py
+++ problemB2_linearised.py
@@ -3,40 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sym
 
-# Define all necessary symbols
+# Define all necessary symbols to calculate derivatives
 x1, x2, x3, x1e, x2e, x3e, c, m, g, k, d, b, V, Ve, R, L0, L1, delta, alpha = sym.symbols(
     'x1, x2, x3, x1e, x2e, x3e, c, m, g, k, d, b, V, Ve, R, L0, L1, delta, alpha')
 
-X1, V, p, u, s, t, sig1, d, q = sym.symbols('X1, V, p, u, s, t, sig1, d, q')
-G = p * u
-G /= ((s ** 2 - (t * s) - sig1) * (s ** 2 - d)) - p * q
-# sym.pprint(G)
-print()
-
-# print(sym.latex(G.factor()))
-print()
-
-"""
-x1. = x2
-x2. = phi
-x3. = psi
-"""
 # Define phi
 phi = (c * x3 ** 2 / (delta - x1) ** 2) - (c * x3e ** 2 / (delta - x1e) ** 2) - k * (x1 - x1e) - b * (x2 - x2e)
 phi *= 5 / (7 * m)
-# sym.pprint(phi)
 
 # Define psi
 psi = (V - x3 * R) / (L0 + L1 * sym.exp(-alpha * (delta - x1)))
 psi -= (Ve - x3e * R) / (L0 + L1 * sym.exp(-alpha * (delta - x1e)))
-# sym.pprint(psi)
+
 
 # Differentiate phi wrt x1, x2, x3
 d_phi_x1 = phi.diff(x1)
 d_phi_x2 = phi.diff(x2)
 d_phi_x3 = phi.diff(x3)
 
-# print()
 
 # Differentiate psi wrt V, x1, x3
 d_psi_V = psi.diff(V)
@@ -47,17 +31,9 @@ d_psi_x3 = psi.diff(x3)
 def evaluate_at_equilibrium_phi(f):
     """
     This function evaluates the equation
-    at the x1=0, x2=0 and x3=0
+    at the x2=0
     """
     return f.subs([(x2, 0)])
-
-
-def evaluate_at_equilibrium_psi(f):
-    """
-    This function evaluates the equation
-    at the V=0, x1=0 and x3=0
-    """
-    return f.subs([])
 
 
 # Evaluate phi at equilibrium point
@@ -65,33 +41,10 @@ d_phi_x1_eq = evaluate_at_equilibrium_phi(d_phi_x1)
 d_phi_x2_eq = evaluate_at_equilibrium_phi(d_phi_x2)
 d_phi_x3_eq = evaluate_at_equilibrium_phi(d_phi_x3)
 
-# Evaluate psi at equilibrium point
-d_psi_V_eq = evaluate_at_equilibrium_psi(d_psi_V)
-d_psi_x1_eq = evaluate_at_equilibrium_psi(d_psi_x1)
-d_psi_x3_eq = evaluate_at_equilibrium_psi(d_psi_x3)
-
-"""
-# Print partial derivatives of phi
-print('d_phi_x1:')
-sym.pprint(d_phi_x1_eq)
-print('d_phi_x2:')
-sym.pprint(d_phi_x2_eq)
-print('d_phi_x3:')
-sym.pprint(d_phi_x3_eq)
-# Print partial derivatives of psi
-print('d_psi_V:')
-sym.pprint(d_psi_V)
-print('d_psi_x1:')
-sym.pprint(d_psi_x1)
-print('d_psi_x3:')
-sym.pprint(d_psi_x3)
-"""
-
 # End of derivatives calculation
 
 
 # Constants defined for linearised system
-
 A_1 = d_phi_x1_eq
 A_2 = d_phi_x2_eq
 A_3 = d_phi_x3_eq
@@ -100,12 +53,9 @@ B_2 = d_psi_x1
 B_3 = d_psi_x3
 
 
-# Define linearised system variables
-# x_1_bar, x_2_bar, x_3_bar, V_bar = sym.symbols('x_1_bar, x_2_bar, x_3_bar, V_bar')
-
-
 class WoodenBall:
 
+    # Declared the values of constants
     def __init__(self,
                  m_value=0.425,  # m
                  g_value=9.81,  # g
@@ -159,28 +109,33 @@ class WoodenBall:
             x_3a = z[2]
             V_value = voltage
 
-            # print(x_1)
-            # print(x_3)
-
             # Subbing the values into the constants: A_1, A_2 ... etc
-            A_1_sub = A_1.subs([(m, self.__mass), (c, self.__electromagnet_constant), (delta, self.__magnet_position),
-                                (k, self.__spring_stiffness), (x3, x_3a), (x1, x_1a)])
-            A_2_sub = A_2.subs([(b, self.__damper_coeff), (m, self.__mass)])
-            A_3_sub = A_3.subs([(c, self.__electromagnet_constant), (m, self.__mass), (delta, self.__magnet_position),
-                                (x3, x_3a), (x1, x_1a)])
-            B_1_sub = B_1.subs([(L0, self.__normal_inductance), (L1, self.__inductance_constant),
-                                (alpha, self.__inductance_exp_constant), (delta, self.__magnet_position), (x1, x_1a)])
-            B_2_sub = B_2.subs([(L1, self.__inductance_constant), (alpha, self.__inductance_exp_constant),
-                                (R, self.__resistance), (delta, self.__magnet_position),
-                                (L0, self.__normal_inductance), (x3, x_3a), (x1, x_1a), (V, V_value)])
-            B_3_sub = B_3.subs([(R, self.__resistance), (L0, self.__normal_inductance),
-                                (L1, self.__inductance_constant), (alpha, self.__inductance_exp_constant),
-                                (delta, self.__magnet_position), (x1, x_1a)])
+            def evaluate_constants(f):
+                """
+                This function subs in
+                the constant values into
+                A1, A2, A3, B1, B2 and B3
+                """
+                return f.subs([(m, self.__mass), (R, self.__resistance), (L0, self.__normal_inductance),
+                               (L1, self.__inductance_constant), (alpha, self.__inductance_exp_constant),
+                               (c, self.__electromagnet_constant),
+                               (delta, self.__magnet_position), (k, self.__spring_stiffness),
+                               (b, self.__damper_coeff), (x3, x_3a), (x1, x_1a), (V, V_value)])
 
+            A_1_sub = evaluate_constants(A_1)
+            A_2_sub = evaluate_constants(A_2)
+            A_3_sub = evaluate_constants(A_3)
+            B_1_sub = evaluate_constants(B_1)
+            B_2_sub = evaluate_constants(B_2)
+            B_3_sub = evaluate_constants(B_3)
+
+            # Declaring the bar variables
             x_1_bar = x_1a - self.x_1_e
             x_2_bar = x_2a - self.x_2_e
             x_3_bar = x_3a - self.x_3_e
             V_bar = voltage - self.V_e
+
+            # The equations are calculated using the variables and returned
             return [x_2_bar, (A_1_sub * x_1_bar) + (A_2_sub * x_2_bar) + (A_3_sub * x_3_bar),
                     (B_1_sub * V_bar) + (B_2_sub * x_1_bar) + (B_3_sub * x_3_bar)]
 
@@ -196,34 +151,11 @@ class WoodenBall:
                              z_initial,
                              t_eval=np.linspace(0, dt, __num_points))
 
-        self.x_1 = solution.y[0][-1]  # [-1] represents last element in the array
+        self.x_1 = solution.y[0][-1]
         self.x_2 = solution.y[1][-1]
         self.x_3 = solution.y[2][-1]
 
-        # STEP 4: Plot solutions - plots open one after the other
-
-        """
-        # plot x vs time
-        plt.plot(solution.t, solution.y[0].T)
-        plt.grid()
-        plt.xlabel('Time (s)')
-        plt.ylabel('x_1 (m)')
-        plt.show()
-
-        # plot y vs time
-        plt.plot(solution.t, solution.y[1].T)
-        plt.grid()
-        plt.xlabel('Time (s)')
-        plt.ylabel('x_2')
-        plt.show()
-
-        # plot theta vs time
-        plt.plot(solution.t, solution.y[2].T)
-        plt.grid()
-        plt.xlabel('Time (s)')
-        plt.ylabel('x_3')
-        plt.show()
-        """
+        # STEP 4: Plot solutions
 
         # Subplot is used to plot the 3 graphs in one figure
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
@@ -246,6 +178,5 @@ class WoodenBall:
         plt.show()
 
 
-# Frame of reference determines anticlockwise as +ve
 woody = WoodenBall()
 woody.move(36.04, 2)

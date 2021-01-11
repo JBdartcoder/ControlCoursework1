396cbd82654eb8d2a2676905376f81de8211f51e
JBdartcoder <johnsgboden@gmail.com>
2021-01-11 10:50:38 +0000

    Working code to produce both responses

--- problemB3/main.py
+++ problemB3/main.py
@@ -1,7 +1,6 @@
 import sympy as sym
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy import signal
+from scipy import signal as sig
 
 # Define all involved symbolic variables
 # constants
@@ -10,6 +9,7 @@ m, g, d, delta, r, R, L_0, L_1, alpha, c, k, b, phi = sym.symbols('m, g, d, delt
 x1_eq, x2_eq, x3_eq, V_e = sym.symbols('x1_eq, x2_eq, x3_eq, V_e')
 
 # Declare determined values of A1 -> B3
+# As determined in Part A
 A_1 = (5 / (7 * m)) * ((2 * c * (x3_eq ** 2) / (delta - x1_eq) ** 3) - k)
 A_2 = -(5 * b) / (7 * m)
 A_3 = (10 * c * x3_eq) / (7 * m * ((delta - x1_eq) ** 2))
@@ -32,60 +32,58 @@ c_value = 6.815
 k_value = 1880
 b_value = 10.4
 phi_value = 42
-V_e_value = 36.04
+V_e_value = 36.0588
 
 # State equilibrium values as determined in derivations
-x1_eq_value = 0.47861
+x1_eq_value = 0.47861  # Previously calculated in Part B
 x2_eq_value = 0
 x3_eq_eqn = V_e / R
 x3_eq_value = x3_eq_eqn.subs([(V_e, V_e_value), (R, R_value)])
 
 # Substitute values of the constants into the equations for A_1 -> B_3
-A_1_value = A_1.subs(
-    [(m, m_value), (c, c_value), (x3_eq, x3_eq_value), (delta, delta_value), (x1_eq, x1_eq_value), (k, k_value)])
-A_2_value = A_2.subs([(b, b_value), (m, m_value)])
-A_3_value = A_3.subs([(c, c_value), (m, m_value), (x3_eq, x3_eq_value), (delta, delta_value), (x1_eq, x1_eq_value)])
-B_1_value = B_1.subs(
-    [(L_0, L_0_value), (L_1, L_1_value), (alpha, alpha_value), (delta, delta_value), (x1_eq, x1_eq_value)])
-B_2_value = B_1.subs(
+A_1_value = float(A_1.subs(
+    [(m, m_value), (c, c_value), (x3_eq, x3_eq_value), (delta, delta_value), (x1_eq, x1_eq_value), (k, k_value)]))
+A_2_value = float(A_2.subs([(b, b_value), (m, m_value)]))
+A_3_value = float(
+    A_3.subs([(c, c_value), (m, m_value), (x3_eq, x3_eq_value), (delta, delta_value), (x1_eq, x1_eq_value)]))
+B_1_value = float(B_1.subs(
+    [(L_0, L_0_value), (L_1, L_1_value), (alpha, alpha_value), (delta, delta_value), (x1_eq, x1_eq_value)]))
+B_2_value = float(B_2.subs(
     [(L_0, L_0_value), (L_1, L_1_value), (alpha, alpha_value), (delta, delta_value), (x1_eq, x1_eq_value), (R, R_value),
-     (x3_eq, x3_eq_value), (V_e, V_e_value)])
-B_3_value = B_1.subs(
+     (x3_eq, x3_eq_value), (V_e, V_e_value)]))
+B_3_value = float(B_3.subs(
     [(L_0, L_0_value), (L_1, L_1_value), (alpha, alpha_value), (delta, delta_value), (x1_eq, x1_eq_value),
-     (R, R_value)])
+     (R, R_value)]))
 
 # Use A_1 -> B_3 to determine the coefficients of the numerator
 # and the denominator (from s^3 - s^0 term)
-
 num_value = A_3_value * B_1_value
-s_3_den_value = 1       # coeff. of s^3
-s_2_den_value = -B_3_value - A_2_value      # coeff. of s^2
-s_1_den_value = (B_3_value * A_2_value) - A_1_value     # coeff. of s^1
-s_0_den_value = (B_3_value*A_1_value) - (A_3_value*B_2_value)       # coeff. of s^0
-
-# Declare overall numerator and denominator of transfer function
+s_3_den_value = 1.  # coeff. of s^3
+s_2_den_value = -B_3_value - A_2_value  # coeff. of s^2
+s_1_den_value = (B_3_value * A_2_value) - A_1_value  # coeff. of s^1
+s_0_den_value = (B_3_value * A_1_value) - (A_3_value * B_2_value)  # coeff. of s^0
 num_Gx = [num_value]
 den_Gx = [s_3_den_value, s_2_den_value, s_1_den_value, s_0_den_value]
 
-# Declare the system as a transfer function signal using the numerator and denominator from above
-sys = signal.TransferFunction(num_Gx, den_Gx)
+# Declare overall transfer function
+G_x_tf = sig.TransferFunction(num_Gx, den_Gx)
 
-# Plot the transfer function impulse response against time
-t = np.linspace(0, 1, 1000)   # Up to 1s, 1000 intervals
-t, y = signal.impulse(sys, T=t)
-plt.figure(1)
-plt.plot(t,y,'k-',label="impulse response")
-plt.xlabel('t (seconds)')
-plt.ylabel('y(t) (unit unknown)')
-plt.legend(loc='best')
+# # Use scipy signal library to plot impulse response
+t, y = sig.impulse(G_x_tf)
+plt.figure(2)
+plt.plot(t, y, 'r-')
+plt.title('Impulse response of transfer function')
+plt.xlabel('time (s)')
+plt.ylabel('$x_{1}$ (m)')
+plt.grid()
 plt.show()
 
-# Plot the transfer function step response against time
-t, y = signal.step(sys, T=t)
+# Use scipy signal library to plot step response
+t, y = sig.step(G_x_tf)
 plt.figure(1)
-plt.plot(t,y,'k-',label="step response")
-plt.xlabel('t (seconds)')
-plt.ylabel('y(t) (unit unknown)')
-plt.legend(loc='best')
+plt.plot(t, y, 'g-')
+plt.title('Step response of transfer function')
+plt.xlabel('time (s)')
+plt.ylabel('$x_{1}$ (m)')
+plt.grid()
 plt.show()
-
